d6b75288f7aaec935d8e75ad8cae6c23bdd3e32c
JaimeMatias <matias.raul.jaime@gmail.com>
2019-04-09 20:38:17 -0300

    Sigo comentando

--- ENTROPY.py
+++ ENTROPY.py
@@ -24,21 +24,22 @@ def subconjunto(entrada,indice=0):
     acierto=[]
     for reg in entrada:
         if final==1:# Si final verdadedor tengo que ir a la ultima poscion
-            indice=int(len(reg)-1)
-        elem=reg[indice]
-        i=-1
+            indice=int(len(reg)-1) #obtengo la longitud del registro
+        elem=reg[indice] #a elemento le asigno el contenido en esa posicion
         if elem not in elementos:
-            elementos=elementos+[elem]
-            Valor.extend([0])
-            acierto.extend([[0,0]])
-        for pos in elementos:
+            elementos=elementos+[elem]# Si el elemento no esta en la lista, lo añado a la lista
+            Valor.extend([0])# Creo una posicoin para adjudicar el elemento
+            acierto.extend([[0,0]])# Creo otra posicion en el acierto
+            #
+        i=-1#lo inicio en -1 para que en el bucle no se pase del arreglo
+        for pos in elementos: #sirve para incrementar los valores de valores y de atributos
             i=i+1
-            if(pos==elem):
-                Valor[i]=Valor[i]+1
-                if reg[len(reg)-1]=='yes':
-                    acierto[i][0]=int(acierto[i][0])+1
+            if(pos==elem):# busco que el valor en posción final sea igual al contenido del vecto del elemento
+                Valor[i]=Valor[i]+1# incremento el valor de la posicion
+                if reg[len(reg)-1]=='yes':# Pregunto si  es si o no
+                    acierto[i][0]=int(acierto[i][0])+1# Incremento el valor del acierto
                 else:
-                    acierto[i][1]=int(acierto[i][1])+1
+                    acierto[i][1]=int(acierto[i][1])+1 #Incremento el alor del fallo
     devolver[0]=elementos
     devolver[1]=Valor
     devolver[2]=acierto
@@ -80,7 +81,7 @@ def entropy(entrada,indice=0):
         return result
 
 
-
+#Una simple función que cuenta la cantidad de registro
 def contar_tot(entrada):
     total=0
     for reg in entrada:
