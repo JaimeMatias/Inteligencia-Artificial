5838bef1f6e4ea978481c302b783d41af6a7d6a3
Nicolas <montoyan877@gmail.com>
2019-05-17 13:10:23 -0300

    Revert "Interface Grafica"
    This reverts commit 4544355495ea09066f8e4e4a2cf7649baf0a7c29.

---
+++ Main.py
@@ -0,0 +1,55 @@
+#-*- coding: latin-1 -*-
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+from read import *
+
+class Aplicacion:
+    def __init__(self):
+        self.ventana1=tk.Tk()
+        self.ventana1.title("Arbol de decisión para datos continuos")
+        self.ventana1.geometry("350x400")  #Tamaño
+        self.ventana1.resizable(0,0)
+        self.ventana1.configure(background="light gray")
+
+        self.texto1=tk.Label(self.ventana1, text="Seleccione el archivo donde se encuentra el conjunto de datos:", bg="black", fg="white")
+        self.texto1.pack(fill=tk.X)
+
+        self.btnCargar=tk.Button(self.ventana1, text="Cargar", command=self.abrirArchivo)
+        self.btnCargar.pack(padx="20",pady="10")
+
+        ############Crea Tabla################
+        self.tv = ttk.Treeview(self.ventana1)
+        self.tv['columns'] = ('Eje Y', 'Clase')
+        self.tv.heading("#0", text='Eje X', anchor='center')
+        self.tv.column("#0", anchor="center", width=70)
+        self.tv.heading("Eje Y",text='Eje Y')
+        self.tv.column("Eje Y", anchor="center", width=70)
+        self.tv.heading("Clase",text='Clase')
+        self.tv.column("Clase", anchor="center", width=70)
+        self.Treeview = self.tv
+        self.tv.pack(pady="8")
+        ########################################
+
+        self.btnGenArbol = tk.Button(self.ventana1, text="Generar Arbol de Decision",command=self.gen_graficas)
+        self.btnGenArbol.pack(pady="10")
+
+        self.btnGen2D = tk.Button(self.ventana1, text="Generar Grafico 2D")
+        self.btnGen2D.pack(padx="1",pady="1")
+
+        self.ventana1.mainloop()
+
+    def abrirArchivo(self,nombre):
+        nombrearch=fd.askopenfilename(initialdir = "/",title = "Seleccione archivo",filetypes = (('.csv', '.csv'),("todos los archivos","*.*")))
+        if nombrearch!='':  #Si se apreto cancel devuelve String vacio ""
+
+            ################# Muestra Archivo en tabla #####################
+            archivo = read_ar(nombrearch)
+            print(nombrearch)
+            for reg in archivo[1]:
+                self.tv.insert('', 'end', text=reg[0], values=(reg[1],reg[2]))
+            ##############################################################
+
+    def gen_grafica(self):
+nombre=''
+aplicacion1=Aplicacion()
