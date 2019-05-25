b8a558d9c24363721ee8381d2f9227f7f6ba2a50
MatiasJaime <matias.raul.jaime@gmail.com>
2019-05-25 17:04:20 -0300

    Comenté mejor e hice que las variables sean mas significativas

--- Desicion/desicion.py
+++ Desicion/desicion.py
@@ -7,7 +7,7 @@ from Entropy import ENTROPY as en
 from copy import deepcopy
 
 
-def decision_tree(archivo, atributos, arbol=None, nivel=0,ganancia=0):
+def decision_tree(archivo, atributos, arbol=None, nivel=0, ganancia=0):
     """
     la implementaciï¿½n del algoritmo c4.5
     :param archivo: un arreglo con todos los datos
@@ -17,89 +17,89 @@ def decision_tree(archivo, atributos, arbol=None, nivel=0,ganancia=0):
 
     :return: un arbol de decisiï¿½n
     """
-    entropiac = en.entropiaclase(archivo)
-    if entropiac == 0:
-        nombre = en.clase(archivo)  # funcion de ENTROPY
-        nod1 = ab.Nodo(nombre, 0, nivel, len(archivo), 1, 'si')
-        arbol = nod1
-        return arbol
-    else:
-        var4 = ['conjunto 1', 'conjunto 2']
-        posicion = [0, 0, 0]
-        valor = 0
+    entropiac = en.entropiaclase(archivo)  # Calculo la entropia del archivo
+    if entropiac == 0:  # Condición de parada, el conjunto de datos es puro
+        nombre = en.clase(archivo)  # determino la clase del conjunto
+        nuevo_nodo = ab.Nodo(nombre=nombre, corte=0, nivel=nivel, soporte=len(archivo), confianza=1,
+                             hoja='si')  # Genero el Nodo
+        arbol = nuevo_nodo  # Lo asigno al arbol
+        return arbol  # Devuelvo el nodo
+    else:  # Trato a los datos continuos oomo conjuntos de datos
+        nombre_conjunto = ['conjunto 1', 'conjunto 2']  # Defino los 2 conjuntos
+        posicion = [0, 0]  # Primer posición, que eje, Segunda posicion desplazamiento dentro del conjunto de datos
+        ganancia_inf = 0  # Ganancia de Información
         for i in range(0, 2):
-            corte = 0
-            atributo = i
-            control = False
-            archivo = rd.ordenar(archivo, atributo)
-
+            valor_corte = 0  # Valor de corte es decir el promedio de los 2 puntos
+            atributo_pos = i  # la poscición del atributo
+            archivo = rd.ordenar(archivo, atributo_pos)  # Ordeno el archivo por el atributo
+            control = False  # Seteo Variable Corntro
             while not control:
-                var1 = rd.genintervalo(archivo, corte, atributo, var4)
-                var2 = gn.gain(var1, atributo)
-                if var2 > valor:
-                    valor = var2
-                    if i == 0:
-                        posicion[1] = corte
+                sub_intervalo = rd.genintervalo(archivo, valor_corte, atributo_pos,
+                                                nombre_conjunto)  # Genero el sib intervalo
+                ganancia_sub_intervalo = gn.gain(sub_intervalo,
+                                                 atributo_pos)  # Generola ganancia del intervalo anterior
+                if ganancia_sub_intervalo > ganancia_inf:  # Pregunto si el nuevo corte, mejora la ganancia de información
+                    ganancia_inf = ganancia_sub_intervalo  # Actualizao la ganancia
+                    if i == 0:  # Estamos en el primer bucle
+                        posicion[1] = valor_corte  # Actualizo la poscion del corte, desplazamientos
                     else:
-                        posicion[0] = 1
-                        posicion[1] = corte
-                corte = rd.genproxcorte(archivo, corte, atributo)
-                if corte == -1:
+                        posicion[0] = 1  # Actualizo la poscion del corte, atributo
+                        posicion[1] = valor_corte  # Actualizo la posicion del corte, desplazamiento
+                valor_corte = rd.genproxcorte(archivo, valor_corte,
+                                              atributo_pos)  # Genero la proxima posicion del corte
+                if valor_corte == -1:  # Overflow, no hay mas posiciones
                     control = True
-        archivo = rd.ordenar(archivo, posicion[0])
-        #print('archivo ordenado:',archivo)
-        posicion[2] = rd.valorcorte(archivo, posicion[1], posicion[0])
+        archivo = rd.ordenar(archivo, posicion[
+            0])  # ordeno el archivo por el atributo que me genera la mayor ganancia de informcion
+        valor_corte = rd.valorcorte(archivo, posicion[1],
+                                    posicion[0])  # Obtengo el valor del corte que me genera la mayor ganancia
 
-        conjunto1 = deepcopy(archivo[0:posicion[1]])
-        conjunto2 = deepcopy(archivo[posicion[1]:])
-        #print(conjunto1)
-        #print(conjunto2)
-        nombre = atributos[posicion[0]]
-        corte = posicion[2]
-        diferencia=valor-ganancia
-
-        ganancia=valor
-        if valor >0:
-            nod2 = ab.Nodo(nombre, corte, nivel, len(archivo))
-            arbol.genelemento(nod2)
-            if conjunto1 is not None:
-                #print(len(conjunto1),conjunto1)
-                #print()
-                nod1 = ab.Nodo(nombre, None, None)
-                nod1 = decision_tree(conjunto1, atributos, nod1, nivel + 1,ganancia)
-                arbol.agregarizq(nod1)
-            if conjunto2 is not None:
-                #print(len(conjunto2),conjunto2)
-                #print()
-                nod1 = ab.Nodo(nombre, None, None)
-                nod1 = decision_tree(conjunto2, atributos, nod1, nivel + 1,ganancia)
-
-                arbol.agregarder(nod1)
-        else:
-            clase1 = en.clase(archivo)
-            #print('nodo hoja: ', clase1)
-            confianza = float(round(en.confianzaclase(archivo, clase1), 2))
-            nod2 = ab.Nodo(clase1, 0, nivel, len(archivo), confianza, 'si')
+        subconjunto_izq = deepcopy(
+            archivo[0:posicion[1]])  # Genero el primer subconjunto, todo lo que está a la izquierda
+        subconjunto_der = deepcopy(
+            archivo[posicion[1]:])  # Genero el segundo subconjunto, todo lo que está a la izquierda
+        nombre = atributos[posicion[0]]  # nombre del eje
+        ganancia = ganancia_inf
+        if ganancia_inf > 0:
+            nod2 = ab.Nodo(nombre, valor_corte, nivel, len(archivo))  # Genero el nodo raiz para esta iteracion
+            arbol.genelemento(nod2)  # Lo agrego al arbol
+            if subconjunto_izq is not None:  # Pregunto si el subconjunto no está vacio
+                nuevo_nodo_izq = ab.Nodo()  # Creo el nodo
+                nuevo_nodo_izq = decision_tree(archivo=subconjunto_izq, atributos=atributos, arbol=nuevo_nodo_izq,
+                                               nivel=nivel + 1, ganancia=ganancia)  # llamo a la funcion recursiva
+                arbol.agregarizq(nuevo_nodo_izq)  # Lo agrego al arbol
+            if subconjunto_der is not None:
+                nuevo_nodo_der = ab.Nodo()
+                nuevo_nodo_der = decision_tree(archivo=subconjunto_der, atributos=atributos, arbol=nuevo_nodo_der,
+                                               nivel=nivel + 1, ganancia=ganancia)
+                arbol.agregarder(nuevo_nodo_der)
+        else:  # Nodos impuros
+            clase1 = en.clase(archivo)  # Busco la clase mas probable
+            confianza = float(round(en.confianzaclase(archivo, clase1), 2))  # Calculo su confianza
+            nod2 = ab.Nodo(nombre=clase1, corte=0, nivel=nivel, soporte=len(archivo), confianza=confianza,
+                           hoja='si')  # Genero el nodo impuro
             arbol = nod2
         return arbol
 
 
-def principal(archivo,nodo):
+def principal(archivo, nodo):
     """
     Es la funciï¿½n mas amplia y que engloba el programa
     :param archivo: la direccion del archivo
     :return: nada
     """
 
-    archivo = rd.read_ar(archivo) # Transforma el archivo csv a una estructura en memoria
+    archivo = rd.read_ar(archivo)  # Transforma el archivo csv a una estructura en memoria
     nivel = 0  # Genera la profundidad inicial
     nodo = decision_tree(archivo[1], archivo[0], nodo, nivel)  # Llama al algoritmo de Decisiï¿½n
 
     nombre = 'Arbol_Decision.png'
-    ab.plot(nodo, nombre) # Plotea otro grafico
-    gf.plotear(archivo[2], archivo[1], nodo, 'grafica_desintegracion.png') #Plotea un grafio
+    ab.plot(nodo, nombre)  # Plotea otro grafico
+    gf.plotear(archivo[2], archivo[1], nodo, 'grafica_desintegracion.png')  # Plotea un grafio
     return nodo
-#nodo=ab.Nodo()
-#print('Creo Nodo')
-#principal('datos3.csv',nodo)
-#nodo.clasepunto(1,1.5)
+
+
+nodo = ab.Nodo()
+print('Creo Nodo')
+principal('datos1.csv', nodo)
+nodo.clasepunto(1, 1.5)
